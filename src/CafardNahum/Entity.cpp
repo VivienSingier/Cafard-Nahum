55c09f184345904bbae1d43a68c6e604b5de6a32
lgarrel <lgarrel@gaming.tech>
2024-12-10 16:25:58 +0100

    fdp

--- src/CafardNahum/Entity.cpp
+++ src/CafardNahum/Entity.cpp
@@ -1,2 +1,13 @@
 #include "pch.h"
 #include "Entity.h"
+
+Entity::Entity(float PosX, float PosY, float SheetX, float SheetY, float height, float width, std::string path, float ScaleX, float ScaleY)
+{
+    if (!texture.loadFromFile(path, sf::IntRect(SheetX, SheetY, width, height)))
+    {
+        return;
+    }
+    sprite.setTexture(texture);
+    sprite.setPosition(PosX, PosY);
+    sprite.setScale(sf::Vector2f(ScaleX, ScaleY));
+}
--- src/CafardNahum/Entity.h
+++ src/CafardNahum/Entity.h
@@ -8,6 +8,7 @@ class Entity : sf::Transformable, sf::Drawable
 	sf::Texture texture;
 
 public:
+	Entity(float PosX, float PosY, float SheetX, float SheetY, float height, float width, std::string path, float ScaleX, float ScaleY);
 	virtual void Update(float deltatime);
 	virtual void Draw(sf::RenderTarget& target, sf::RenderStates states);
 
