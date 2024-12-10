9c17b4dc3298c7ad21283dfba4f333d4175ab8b7
Vivien Singier <vsingier@gaming.tech>
2024-12-10 17:00:59 +0100

    feziuhiurzkghuriezjkg

--- src/CafardNahum/Enemy.cpp
+++ src/CafardNahum/Enemy.cpp
@@ -1,2 +1,11 @@
 #include "pch.h"
 #include "Enemy.h"
+
+Enemy::Enemy(std::string path, sf::IntRect textureRect, sf::Vector2f position, sf::Vector2f scale, sf::Vector2f speed, int cHealth) :
+	Entity::Entity(path, textureRect, position, scale),
+	Movable::Movable(speed),
+	Alive::Alive(cHealth),
+	ColliderSphere::ColliderSphere()
+{
+
+}
--- src/CafardNahum/Enemy.h
+++ src/CafardNahum/Enemy.h
@@ -10,6 +10,8 @@ class Enemy : public Entity, public Alive, public Movable, public ColliderSphere
 	int damage;
 
 public:
+	Enemy(std::string path, sf::IntRect textureRect, sf::Vector2f position, sf::Vector2f scale, sf::Vector2f speed, int cHealth);
+
 	void Move(float deltatime) override;
 
 	void GetShotAngle();
