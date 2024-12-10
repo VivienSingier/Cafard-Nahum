a4c9f56702129ee13b8e6efbe4c04d5c6b98cad1
Vivien Singier <vsingier@gaming.tech>
2024-12-10 17:47:58 +0100

    feGEZgzrG

--- src/CafardNahum/StaticObject.cpp
+++ src/CafardNahum/StaticObject.cpp
@@ -1,2 +1,8 @@
 #include "pch.h"
 #include "StaticObject.h"
+
+StaticObject::StaticObject(std::string path, sf::IntRect textureRect, sf::Vector2f position, sf::Vector2f scale) :
+	Entity(path, textureRect, position, scale)
+{
+
+}
--- src/CafardNahum/StaticObject.h
+++ src/CafardNahum/StaticObject.h
@@ -5,5 +5,7 @@
 class StaticObject : public Entity, public ColliderRect
 {
 	bool IsWalkable;
+public:
+	StaticObject(std::string path, sf::IntRect textureRect, sf::Vector2f position, sf::Vector2f scale);
 };
 
